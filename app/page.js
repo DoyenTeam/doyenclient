eedd9474c6470f3eda6ef9ec6ae82bcf8ee18943
emlag <edl926@g.harvard.edu>
2023-03-08 07:48:27 -0500

    add initial UI iteration. Currently it doesn't pull data and uses statics labels.

--- app/page.js
+++ app/page.js
@@ -33,10 +33,10 @@ export default function Home() {
                 </label>
                 <div className="mt-2">
                   <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
+                    id="text"
+                    name="text"
+                    type="text"
+                    autoComplete="text"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
@@ -45,17 +45,17 @@ export default function Home() {
 
               <div>
                 <label
-                  htmlFor="password"
+                  htmlFor="text"
                   className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Search Context
                 </label>
                 <div className="mt-2">
                   <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
+                    id="text"
+                    name="text"
+                    type="text"
+                    autoComplete="current-text"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
