7623b1bf6f21f2599b91a89b86e25b4cc5ce7c10
emlag <edl926@g.harvard.edu>
2023-04-10 17:33:12 -0400

    add documentation for results

--- app/results/page.js
+++ app/results/page.js
@@ -3,6 +3,11 @@ import Publications from "@/app/results/Publications";
 import {useSearchParams} from "next/navigation";
 import {console} from "next/dist/compiled/@edge-runtime/primitives/console";
 
+/**
+ * The resultsPage is a sever component that wraps the Results component. Since publications is a client
+ * and Results is a server component, next.js doesn't allow them a client component to be called directly in a
+ * server component. To get around this issue, vercel/next.js recommend this wrapper.
+ */
 export default function resultsPage({searchParams}){
     console.log("PARAMS")
     console.log(searchParams['q'])
