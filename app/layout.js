3861bad88076b7c43e586fe4336845dc1bd7b401
emlag <edl926@g.harvard.edu>
2023-04-28 19:23:31 -0400

    add auth

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,27 @@
 import './globals.css'
 
+//used to obtain the session from firebase and next-auth
+import {SessionProvider} from "@/app/SessionProvider";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/pages/api/auth/[...nextauth]";
+
 export const metadata = {
   title: 'Doyen',
   description: 'Search for an Expert',
 }
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+
+    //this session will return an object when the user signs in
+  const session = await getServerSession(authOptions)
+
   return (
     <html lang="en">
-      <body>{children}</body>
+        <body>
+            <SessionProvider session={session}>
+                {children}
+            </SessionProvider>
+        </body>
     </html>
   )
 }
