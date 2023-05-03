aee45cba8611f65ba216c09ac2cd74bf277d761f
emlag <edl926@g.harvard.edu>
2023-05-02 20:06:09 -0400

    update sign in

--- app/Search.js
+++ app/Search.js
@@ -68,7 +68,9 @@ export default function Search() {
                   </div>
 
                   <div className="mt-6 flex flex-col items-center">
-                    <button type="button" onClick={() => signIn("google")}
+                    <button type="button" onClick={async () => {
+                      await signIn("google")
+                    }}
                             className="text-white bg-[#bdc3c7] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
                       <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab"
                            data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
